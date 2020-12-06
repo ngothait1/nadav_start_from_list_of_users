6d027e1a5976d833f9702a613f9085337d686e33
ngothait1 <nadavgot@gmail.com>
2020-12-06 15:17:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ngfor-display-items-example-8pkt25
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ngfor-display-items-example-8pkt25)
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent {
+    users = [
+        { firstName: 'Frank', lastName: 'Murphy', email: 'frank.murphy@test.com', role: 'User' },
+        { firstName: 'Vic', lastName: 'Reynolds', email: 'vic.reynolds@test.com', role: 'Admin' },
+        { firstName: 'Gina', lastName: 'Jabowski', email: 'gina.jabowski@test.com', role: 'Admin' },
+        { firstName: 'Jessi', lastName: 'Glaser', email: 'jessi.glaser@test.com', role: 'User' },
+        { firstName: 'Jay', lastName: 'Bilzerian', email: 'jay.bilzerian@test.com', role: 'User' }
+    ];
+}
